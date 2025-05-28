be71ba20ca9a23eeea0bf9bee12a4c97faf5b8e8
Đức <161590349+tvangasa@users.noreply.github.com>
2025-05-28 15:31:34 +0700

    Update GameUI.java

--- Minesweeper.java/GameUI.java
+++ Minesweeper.java/GameUI.java
@@ -72,3 +72,97 @@ public class GameUI {
 		resetButton.setBackground(Color.LIGHT_GRAY);
 
 	}
+public void setupBoard() {
+		MineTile[][] tiles = board.getBoard();
+		for (int r = 0; r < board.getNumRows(); r++) {
+			for (int c = 0; c < board.getNumCols(); c++) {
+				MineTile tile = tiles[r][c];
+				tile.setFocusable(false);
+				tile.setMargin(new Insets(0, 0, 0, 0));
+				tile.setFont(new Font("Arial Unicode Ms", Font.PLAIN, 45));
+				boardPanel.add(tile);
+
+				tile.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mousePressed(MouseEvent e) {
+						if (gameOver)
+							return;
+
+						if (e.getButton() == MouseEvent.BUTTON1) {
+							if (tile.getText().equals("")) {
+								if (board.isMine(tile)) {
+									revealMines();
+								} else {
+									checkMine(tile.getRow(), tile.getCol());
+								}
+							}
+						} else if (e.getButton() == MouseEvent.BUTTON3) {
+							if (tile.getText().equals("") && tile.isEnabled()) {
+								tile.setText("🚩");
+							} else if (tile.getText().equals("🚩")) {
+								tile.setText("");
+							}
+						}
+					}
+				});
+			}
+		}
+		frame.setVisible(true);
+	}
+
+	private void revealMines() {
+		for (MineTile tile : board.getMineList()) {
+			tile.setText("💣");
+		}
+		gameOver = true;
+		textLabel.setText("Game Over!");
+		timer.stop();
+	}
+
+	private void checkMine(int r, int c) {
+		if (r < 0 || r >= board.getNumRows() || c < 0 || c >= board.getNumCols())
+			return;
+
+		MineTile tile = board.getTile(r, c);
+		if (!tile.isEnabled())
+			return;
+
+		tile.setEnabled(false);
+		tilesClicked++;
+
+		int minesFound = 0;
+		// top 3
+		minesFound += countMine(r - 1, c - 1); // top left
+		minesFound += countMine(r - 1, c); // top
+		minesFound += countMine(r - 1, c + 1); // top right
+		// left and right
+		minesFound += countMine(r, c - 1); // left
+		minesFound += countMine(r, c + 1); // right
+		// bottom
+		minesFound += countMine(r + 1, c - 1); // bottom left
+		minesFound += countMine(r + 1, c); // bottom
+		minesFound += countMine(r + 1, c + 1); // bottom right
+
+		if (minesFound > 0) {
+			tile.setText(Integer.toString(minesFound));
+		} else {
+			tile.setText("");
+
+			// top 3
+			checkMine(r - 1, c - 1); // top left
+			checkMine(r - 1, c); // top
+			checkMine(r - 1, c + 1); // top right
+			// left and right
+			checkMine(r, c - 1); // left
+			checkMine(r, c + 1); // right
+			// bottom 3
+			checkMine(r + 1, c - 1); // bottom left
+			checkMine(r + 1, c); // bottom
+			checkMine(r + 1, c + 1); // bottom right
+		}
+		if (tilesClicked == board.getNumRows() * board.getNumCols() - board.getMineList().size()) {
+			gameOver = true;
+			textLabel.setText("Mines Cleared!");
+			timer.stop();
+		}
+	}
