afc9f0d6f041285449d34693f9c3fd83059d63f0
Đức <161590349+tvangasa@users.noreply.github.com>
2025-06-21 04:24:27 +0000

    update

--- Minesweeper/Main.java
+++ Minesweeper/Main.java
@@ -2,9 +2,16 @@ package Minesweeper;
 
 public class Main {
 	public static void main(String[] args) {
-		MineBoard board = new MineBoard(8, 8, 10);
-		GameUI ui = new GameUI(board);
-		Gamecontroller controller = new Gamecontroller(board, ui);
-		controller.startGame();
+		try {
+			MineBoard board = new MineBoard(8, 8, 10);
+			GameUI ui = new GameUI(board);
+			Gamecontroller controller = new Gamecontroller(board, ui);
+			controller.startGame();
+		} catch (Exception e) {
+			System.out.println("Đã xảy ra lỗi trong quá trình khởi động trò chơi: " + e.getMessage());
+			e.printStackTrace(); // Giúp in lỗi chi tiết để dễ sửa
+		} finally {
+			System.out.println("Chương trình main() đã kết thúc.");
+		}
 	}
 }
